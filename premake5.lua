17ed82a71cf09381b41d8d1ab22432fd9643328b
iabxhtk <stawikowski.lukasz@gmail.com>
2020-10-18 23:28:27 +0200

    Premake file

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+    filter "system:windows"
+        systemversion "latest"
+	filter "configurations:DevDebug"
+		runtime "Debug"
+		symbols "On"
+	filter "configurations:LocalDev"
+		runtime "Debug"
+		symbols "On"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
